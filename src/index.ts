81296471bceb61c3132b83b50d1ba97235f662c8
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-12-14 11:13:57 -0800

    chore: format using prettier

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { AwsLambdaAsyncInitTimestampsStack } from './aws-lambda-async-init-timestamps-stack';
+import * as cdk from "aws-cdk-lib";
+import { AwsLambdaAsyncInitTimestampsStack } from "./aws-lambda-async-init-timestamps-stack";
 
 const app = new cdk.App();
-new AwsLambdaAsyncInitTimestampsStack(app, 'AwsLambdaAsyncInitTimestamps');
+new AwsLambdaAsyncInitTimestampsStack(app, "AwsLambdaAsyncInitTimestamps");
